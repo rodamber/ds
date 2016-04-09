594c4a8e0f2026779a92670965df805ac6b5880b
Mariana Ribeiro <marianasofiaribeiro@tecnico.ulisboa.pt>
2016-04-09 19:35:00 +0100

    Developed client, its application and exceptions. Replaced tabs with spaces.

--- transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
@@ -1,7 +1,155 @@
 package pt.upa.transporter.ws.cli;
 
-public class TransporterClient {
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 
-	// TODO
+import java.util.Map;
 
+import javax.xml.ws.BindingProvider;
+
+import pt.upa.transporter.ws.*;
+import java.util.List;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+
+public class TransporterClient implements TransporterPortType {
+
+    /** WS service */
+    TransporterService service = null;
+
+    /** WS port (port type is the interface, port is the implementation) */
+    TransporterPortType port = null;
+
+    /** UDDI server URL */
+    private String uddiURL = null;
+
+    /** WS name */
+    private String wsName = null;
+
+    /** WS endpoint address */
+    private String wsURL = null; // default value is defined inside WSDL
+
+    public String getWsURL() {
+        return wsURL;
+    }
+
+    /** output option **/
+    private boolean verbose = false;
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    /** constructor with provided web service URL */
+    public TransporterClient(String wsURL) throws TransporterClientException {
+        this.wsURL = wsURL;
+        createStub();
+    }
+
+    /** constructor with provided UDDI location and name */
+    public TransporterClient(String uddiURL, String wsName) throws TransporterClientException {
+        this.uddiURL = uddiURL;
+        this.wsName = wsName;
+        uddiLookup();
+        createStub();
+    }
+
+    /** UDDI lookup */
+    private void uddiLookup() throws TransporterClientException {
+        try {
+            if (verbose)
+                System.out.printf("Contacting UDDI at %s%n", uddiURL);
+            UDDINaming uddiNaming = new UDDINaming(uddiURL);
+
+            if (verbose)
+                System.out.printf("Looking for '%s'%n", wsName);
+            wsURL = uddiNaming.lookup(wsName);
+
+        } catch (Exception e) {
+            String msg = String.format("Client failed lookup on UDDI at %s!", uddiURL);
+            throw new TransporterClientException(msg, e);
+        }
+
+        if (wsURL == null) {
+            String msg = String.format("Service with name %s not found on UDDI at %s", wsName, uddiURL);
+            throw new TransporterClientException(msg);
+        }
+    }
+
+    /** Stub creation and configuration */
+    private void createStub() {
+        if (verbose)
+            System.out.println("Creating stub ...");
+        service = new TransporterService();
+        port = service.getTransporterPort();
+
+        if (wsURL != null) {
+            if (verbose)
+                System.out.println("Setting endpoint address ...");
+            BindingProvider bindingProvider = (BindingProvider) port;
+            Map<String, Object> requestContext = bindingProvider.getRequestContext();
+            requestContext.put(ENDPOINT_ADDRESS_PROPERTY, wsURL);
+        }
+    }
+
+    /* *********************************************
+    
+    CALC EXAMPLE
+
+    public int sum(int a, int b) {
+        return port.sum(a, b);
+    }
+
+    public int sub(int a, int b) {
+        return port.sub(a, b);
+    }
+
+    public int mult(int a, int b) {
+        return port.mult(a, b);
+    }
+
+    public int intdiv(int a, int b) throws DivideByZero {
+        return port.intdiv(a, b);
+    }
+    
+    ********************************************* */
+    
+    /* TransporterPortType implementation */
+
+    @Override
+    public String ping(String name) {
+        return "Ping: " + name;
+    }
+
+    @Override
+    public JobView requestJob(String origin, String destination, int price)
+        throws BadLocationFault_Exception, BadPriceFault_Exception {
+        /* TODO */
+        return null;
+    }
+
+    @Override
+    public JobView decideJob(String id, boolean accept)
+        throws BadJobFault_Exception {
+        /* TODO */
+        return null;
+    }
+
+    @Override
+    public JobView jobStatus(String id) {
+        /* TODO */
+        return null;
+    }
+
+
+    @Override
+    public List<JobView> listJobs() {
+        /* TODO */
+        return null;
+    }
+
+    @Override
+    public void clearJobs() { /* TODO */ }
 }
--- transporter-ws/src/main/java/pt/upa/transporter/TransporterApplication.java
+++ transporter-ws/src/main/java/pt/upa/transporter/TransporterApplication.java
@@ -6,36 +6,36 @@ import pt.upa.transporter.ws.*;
 
 public class TransporterApplication {
 
-	public static void main(String[] args) throws Exception {
-		// Check arguments
-		if (args.length == 0 || args.length == 2) {
-			System.err.println("Argument(s) missing!");
-			System.err.println("Usage: java " + TransporterApplication.class.getName() + " wsURL OR uddiURL wsName wsURL");
-			return;
-		}
-		String uddiURL = null;
-		String wsName = null;
-		String wsURL = null;
+    public static void main(String[] args) throws Exception {
+        // Check arguments
+        if (args.length == 0 || args.length == 2) {
+            System.err.println("Argument(s) missing!");
+            System.err.println("Usage: java " + TransporterApplication.class.getName() + " wsURL OR uddiURL wsName wsURL");
+            return;
+        }
+        String uddiURL = null;
+        String wsName = null;
+        String wsURL = null;
 
-		// Create server implementation object, according to options
-		TransporterEndpointManager endpoint = null;
-		if (args.length == 1) {
-			wsURL = args[0];
-			endpoint = new TransporterEndpointManager(wsURL);
-		} else if (args.length >= 3) {
-			uddiURL = args[0];
-			wsName = args[1];
-			wsURL = args[2];
-			endpoint = new TransporterEndpointManager(uddiURL, wsName, wsURL);
-			endpoint.setVerbose(true);
-		}
+        // Create server implementation object, according to options
+        TransporterEndpointManager endpoint = null;
+        if (args.length == 1) {
+            wsURL = args[0];
+            endpoint = new TransporterEndpointManager(wsURL);
+        } else if (args.length >= 3) {
+            uddiURL = args[0];
+            wsName = args[1];
+            wsURL = args[2];
+            endpoint = new TransporterEndpointManager(uddiURL, wsName, wsURL);
+            endpoint.setVerbose(true);
+        }
 
-		try {
-			endpoint.start();
-			endpoint.awaitConnections();
-		} finally {
-			endpoint.stop();
-		}
-	}
+        try {
+            endpoint.start();
+            endpoint.awaitConnections();
+        } finally {
+            endpoint.stop();
+        }
+    }
 
 }
