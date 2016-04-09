5697efc26d2f1a10103c7cf54097b9b4ce8374a0
rodamber <rodrigoambernardo@gmail.com>
2016-04-09 19:27:53 +0100

    Created BrokerPort.

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,7 +1,58 @@
 package pt.upa.broker.ws;
 
-public class BrokerPort {
+import java.util.List;
+import javax.jws.WebService;
 
-	// TODO
+@WebService(
+    name              = "BrokerWebService",
+    targetNamespace   = "http://ws.broker.upa.pt/",
+    wsdlLocation      = "broker.1_0.wsdl",
+    serviceName       = "BrokerService",
+    portName          = "BrokerPort",
+    endpointInterface = "broker.BrokerPortType"
+)
+public class BrokerPort implements BrokerPortType {
+
+    private BrokerEndpointManager endpoint;
+
+    public BrokerPort(BrokerEndpointManager endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    BrokerPort() { }
+
+    /* BrokerPortType implementation */
+
+    @Override
+    public String ping(String name) {
+        return "Ping: " + name;
+    }
+
+    @Override
+    public String requestTransport(String origin, String destination, int price)
+        throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception,
+               UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+        /* RODRIGO:FIXME:TODO */
+        return null;
+    }
+
+    @Override
+    public TransportView viewTransport(String id) throws UnknownTransportFault_Exception {
+        /* RODRIGO:FIXME:TODO */
+        return null;
+    }
+
+    @Override
+    public List<TransportView> listTransports() {
+        /* RODRIGO:FIXME:TODO */
+        return null;
+    }
+
+    @Override
+    public void clearTransports() {
+        /* RODRIGO:FIXME:TODO */
+    }
 
 }
+
+
