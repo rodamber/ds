dc1718116f75f0d76690ed231cc9d8a4dddcae93
Mariana Ribeiro <marianasofiaribeiro@tecnico.ulisboa.pt>
2016-04-10 18:37:31 +0100

    Added BrokerApplication

--- broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
+++ broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
@@ -1,10 +1,41 @@
 package pt.upa.broker;
 
+import javax.xml.ws.Endpoint;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.upa.broker.ws.*;
+
 public class BrokerApplication {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println(BrokerApplication.class.getSimpleName() + " starting...");
+		// Check arguments
+        if (args.length == 0 || args.length == 2) {
+            System.err.println("Argument(s) missing!");
+            System.err.println("Usage: java " + BrokerApplication.class.getName() + " wsURL OR uddiURL wsName wsURL");
+            return;
+        }
+        String uddiURL = null;
+        String wsName = null;
+        String wsURL = null;
 
+        // Create server implementation object, according to options
+        BrokerEndpointManager endpoint = null;
+        if (args.length == 1) {
+            wsURL = args[0];
+            endpoint = new BrokerEndpointManager(wsURL);
+        } else if (args.length >= 3) {
+            uddiURL = args[0];
+            wsName = args[1];
+            wsURL = args[2];
+            endpoint = new BrokerEndpointManager(uddiURL, wsName, wsURL);
+            endpoint.setVerbose(true);
+        }
+
+        try {
+            endpoint.start();
+            endpoint.awaitConnections();
+        } finally {
+            endpoint.stop();
+        }
 	}
 
 }
