42e63755bc53ccd7a315252ace338048fcd0442c
Bernardo Cordeiro <bernardo.f.cordeiro@tecnico.ulisboa.pt>
2016-04-15 20:00:26 +0100

    removi o utils

--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
@@ -1,9 +1,9 @@
 package pt.upa.transporter.ws;
 
-import java.util.Arrays;
 import java.util.List;
-import pt.upa.transporter.Utils;
-
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.jws.WebService;
 
 @WebService(
@@ -18,6 +18,7 @@ import javax.jws.WebService;
 public class TransporterPort implements TransporterPortType {
 
     private TransporterEndpointManager endpoint;
+    private List<JobView> registry = new ArrayList<JobView>();
     
     public TransporterPort(TransporterEndpointManager endpoint) {
         this.endpoint = endpoint;
@@ -36,19 +37,19 @@ public class TransporterPort implements TransporterPortType {
     public JobView requestJob(String origin, String destination, int price)
         throws BadLocationFault_Exception, BadPriceFault_Exception {
     	
-    	int transporterID = Utils.getTransporterID(endpoint.getWsName());
+    	int transporterID = getTransporterID(endpoint.getWsName());
     	    	
-    	if(!Utils.isCityNameValid(origin)){
+    	if(!isCityNameValid(origin)){
     		throw new BadLocationFault_Exception(origin + " is not a valid city name", new BadLocationFault());
     	}
-    	else if(!Utils.isCityNameValid(destination)){
+    	else if(!isCityNameValid(destination)){
     		throw new BadLocationFault_Exception(origin + " is not a valid city name", new BadLocationFault());
     	}
     	else if(price < 0){
     		throw new BadPriceFault_Exception("Price must be greater than 0", new BadPriceFault());
     	}
     	
-    	if(!Utils.transporterServesCity(transporterID, origin) || !Utils.transporterServesCity(transporterID, destination)){
+    	if(!transporterServesCity(transporterID, origin) || !transporterServesCity(transporterID, destination)){
     		return null;
     	}
     	else if(price > 100){
@@ -91,6 +92,58 @@ public class TransporterPort implements TransporterPortType {
     @Override
     public void clearJobs() {
     	/* RODRIGO:FIXME:TODO */ 
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    private List<String> northRegion = Arrays.asList("Porto", "Braga", "Viana do Castelo", "Vila Real", "Bragança");
+    private List<String> centerRegion = Arrays.asList("Lisboa", "Leiria", "Santarém", "Castelo Branco", "Coimbra", "Aveiro", "Viseu", "Guarda");
+    private List<String> southRegion = Arrays.asList("Setúbal", "Évora", "Portalegre", "Beja", "Faro");
+
+    private boolean isCityNameValid(String cityname){
+        return northRegion.contains(cityname) || centerRegion.contains(cityname) || southRegion.contains(cityname);
+    }
+    
+    private boolean transporterServesCity(int transporterID, String cityname){
+        return ((northRegion.contains(cityname) || centerRegion.contains(cityname)) && (transporterID%2) == 0) || ((centerRegion.contains(cityname) || southRegion.contains(cityname)) && (transporterID%2) == 1);
+    }
+    
+    private int getTransporterID(String transporterWsName){
+        return transporterWsName.charAt(transporterWsName.length()-1);
+    }
+    
+    private int random(int min, int max){
+    	return (new Random()).nextInt((max - min) + 1) + min;
+    }
+    
+    private String generateID(int transporterID, int jobID){
+    	return "UpaTransporter" + Integer.toString(transporterID) + "-" + Integer.toString(jobID);
     }
 
 }
