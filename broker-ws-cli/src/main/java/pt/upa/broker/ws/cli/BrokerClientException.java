804f4c886ccde739a1d35f177f9b8e02755863f7
rodamber <rodrigoambernardo@gmail.com>
2016-05-05 20:27:36 +0100

    Added professors evaluation clients

--- broker-ws-cli/src/main/java/pt/upa/broker/ws/cli/BrokerClientException.java
+++ broker-ws-cli/src/main/java/pt/upa/broker/ws/cli/BrokerClientException.java
@@ -1,10 +1,13 @@
 package pt.upa.broker.ws.cli;
 
+/**
+ *	Exception thrown by the Client when there is an unexpected error condition.  
+ */
 public class BrokerClientException extends Exception {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public BrokerClientException() {
+	public BrokerClientException() {
     }
 
     public BrokerClientException(String message) {
