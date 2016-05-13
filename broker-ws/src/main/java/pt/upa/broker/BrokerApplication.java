14d913fc23c65038bcde113190d938d5dc7f068b
rodamber <rodrigoambernardo@gmail.com>
2016-05-13 01:53:19 +0100

    New xpto broker application

--- broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
+++ broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
@@ -1,44 +1,98 @@
 package pt.upa.broker;
 
-import java.util.Optional;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
-import javax.xml.ws.Endpoint;
-import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import javax.xml.ws.*;
+
+import pt.ulisboa.tecnico.sdis.ws.uddi.*;
 import pt.upa.transporter.ws.cli.*;
 import pt.upa.broker.ws.*;
 
 public class BrokerApplication {
 
     public static void main(String[] args) throws Exception {
-        if (args.length == 0 || args.length == 2) {
-            System.err.println("Argument(s) missing!");
-            System.err.println("Usage: java " + BrokerApplication.class.getName() +
-                               " wsURL OR uddiURL wsName wsURL");
-            return;
-        }
 
-        BrokerEndpointManager endpoint = null;
-        if (args.length == 2) {
-            final String wsURL = args[1];
-            final String mode    = BrokerPort.PRIMARY_MODE;
-            endpoint = new BrokerEndpointManager(wsURL, mode, Optional.empty());
-        } else if (args.length >= 5) {
-            final String uddiURL     = args[1];
-            final String wsName      = args[2];
-            final String wsURL       = args[3];
-            final String mode        = args[4];
-            final String backupWsURL = args[5];
-            endpoint = new BrokerEndpointManager(uddiURL, wsName, wsURL, mode,
-                                                 Optional.ofNullable(backupWsURL));
-        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        endpoint.setVerbose(Boolean.parseBoolean(args[0]));
+        final String verbose = ask(in, "Verbose [true or false]", args, 0);
+        final String mode = ask(in, "Mode [PRIMARY_MODE or BACKUP_MODE]", args, 1);
+        final String uddiIP = ask(in, "UDDI IP", args, 2);
+        final String uddiPort = ask(in, "UDDI Port", args, 3);
+        final String wsName = ask(in, "Web Service Name", args, 4);
+        final String wsIP = ask(in, "This Server IP", args, 5);
+        final String wsPort = ask(in, "This Server Port", args, 6);
+        final String backupWsIP = askNullable(in, "Backup Server IP", args, 7);
+        final String backupWsPort = askNullable(in, "Backup Server Port", args, 8);
+
+        final String uddiURL = buildURL(uddiIP, uddiPort);
+        final String wsURL = buildWsURL(wsIP, wsPort);
+        final String backupWsURL = buildWsURL(backupWsIP, backupWsPort);
+
+        final BrokerEndpointManager endpoint =
+            new BrokerEndpointManager(uddiURL, wsName, wsURL, mode,
+                                      Optional.ofNullable(backupWsURL));
+        endpoint.setVerbose(Boolean.parseBoolean(verbose));
 
         try {
             endpoint.start();
             endpoint.awaitConnections();
         } finally {
             endpoint.stop();
+        }
+    }
+
+    public static String ask(BufferedReader in, String prompt, String[] args,
+                             int index) throws IOException {
+        return ask(in, prompt, args, index, false);
+    }
+
+    public static String askNullable(BufferedReader in, String prompt,
+                                     String[] args, int index) throws IOException {
+        return ask(in, prompt, args, index, true);
+    }
+
+    public static String ask(BufferedReader in, String prompt, String[] args,
+                             int index, boolean nullable) throws IOException {
+        Optional<String> defVal = elemAt(args, index);
+
+        System.out.printf("%s %s:\n> ", prompt,
+                          "(default: " + (defVal.isPresent() ? defVal.get() : "none") + ")");
+
+        final String input = in.readLine();
+        if (input.isEmpty() && !defVal.isPresent() && !nullable) {
+            throw new NullPointerException("Arguments missing!");
+        } else if (input.isEmpty() && defVal.isPresent()){
+            return defVal.get();
+        } else if (input.isEmpty() && nullable) {
+            return null;
+        } else {
+            return input;
+        }
+    }
+
+    public static <T> Optional<T> elemAt(T[] array, int index) {
+        if (array.length <= index) {
+            return Optional.empty();
+        } else {
+            return Optional.ofNullable(array[index]);
+        }
+    }
+
+    public static String buildURL(String ip, String port) {
+        if (ip == null || port == null) {
+            return null;
+        } else {
+            return "http://" + ip + ":" + port;
+        }
+    }
+
+    public static String buildWsURL(String ip, String port) {
+        if (buildURL(ip, port) != null) {
+            return buildURL(ip, port) + "/broker-ws/endpoint";
+        } else {
+            return null;
         }
     }
 
